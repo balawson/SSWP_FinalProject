b8621241fa24db8734184a40ab263c7046f20d67
balawson <89851546+balawson@users.noreply.github.com>
2023-12-03 19:08:56 -0500

    Add files via upload

---
+++ CardService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectFront
+{
+    public class CardService
+    {
+        private readonly HttpClient _httpClient;
+
+        public CardService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<string>> StartGame()
+        {
+            try
+            {
+                var response = await _httpClient.PostAsync("/start-game", null);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadFromJsonAsync<List<string>>();
+            }
+            catch (HttpRequestException)
+            {
+                // Handle exception (e.g., display an error message)
+                return null;
+            }
+        }
+
+        public async Task<List<string>> Hit()
+        {
+            try
+            {
+                var response = await _httpClient.PostAsync("/hit", null);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadFromJsonAsync<List<string>>();
+            }
+            catch (HttpRequestException)
+            {
+                // Handle exception (e.g., display an error message)
+                return null;
+            }
+        }
+
+        public async Task<List<string>> Stand()
+        {
+            try
+            {
+                var response = await _httpClient.PostAsync("/stand", null);
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadFromJsonAsync<List<string>>();
+            }
+            catch (HttpRequestException)
+            {
+                // Handle exception (e.g., display an error message)
+                return null;
+            }
+        }
+    }
+}
