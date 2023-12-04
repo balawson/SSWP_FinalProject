92aa12fda4456a3b8da6d0ba75777817dbfb309f
balawson <89851546+balawson@users.noreply.github.com>
2023-12-04 00:23:00 -0500

    Add files via upload

---
+++ Frontend/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectFront.Data
+{
+        public class Card
+        {
+            public int Id { get; set; }
+            public string Value { get; set; }
+            public string Suit { get; set; }
+
+        public override string ToString()
+            {
+                return $"{Value} of {Suit}";
+            }
+        }
+    
+
+}
